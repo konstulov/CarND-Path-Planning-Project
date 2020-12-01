34ea785b9a56ad0362880ae5f7fb509b8b16cc19
konstulov <konstulov@gmail.com>
2020-12-01 15:16:59 -0800

    Add logic to avoid crashing into the car in front of the SDV.

--- src/main.cpp
+++ src/main.cpp
@@ -51,8 +51,14 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  // start in lane 1
+  int lane = 1;
+
+  // Reference velocity (mph) to target
+  double ref_vel = 0.0;
+
+  h.onMessage([&ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+               &map_waypoints_dx,&map_waypoints_dy, &lane]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -64,12 +70,12 @@ int main() {
 
       if (s != "") {
         auto j = json::parse(s);
-        
+
         string event = j[0].get<string>();
-        
+
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
+
           // Main car's localization Data
           double car_x = j[1]["x"];
           double car_y = j[1]["y"];
@@ -81,21 +87,48 @@ int main() {
           // Previous path data given to the Planner
           auto previous_path_x = j[1]["previous_path_x"];
           auto previous_path_y = j[1]["previous_path_y"];
-          // Previous path's end s and d values 
+          // Previous path's end s and d values
           double end_path_s = j[1]["end_path_s"];
           double end_path_d = j[1]["end_path_d"];
 
-          // Sensor Fusion Data, a list of all other cars on the same side 
+          // Sensor Fusion Data, a list of all other cars on the same side
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
-          // start in lane 1
-          int lane = 1;
-
-          // Reference velocity (mph) to target
-          double ref_vel = 49.5;
-
           int prev_size = previous_path_x.size();
+
+          if (prev_size > 0) {
+            car_s = end_path_s;
+          }
+
+          bool too_close = false;
+
+          // find ref_v to use
+          for (int i=0; i<sensor_fusion.size(); i++) {
+            // car is in my lane
+            float d = sensor_fusion[i][6];
+            double lane_d = 2 + 4*lane;
+            if (d < lane_d+2 && d > lane_d-2) {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx*vx + vy*vy);
+              double check_car_s = sensor_fusion[i][5];
+
+              check_car_s += ((double)prev_size*0.02*check_speed);
+              if ((check_car_s > car_s) && ((check_car_s - car_s) < 30)) {
+                // do some logic here, lower ref velocity so we don't crash into the car in front of us
+                // could aslo flag to try to change lanes
+                //ref_vel = 29.5; // mph
+                too_close = true;
+              }
+            }
+          }
+
+          if (too_close) {
+            ref_vel -= 0.224; // about 5 m/s^2
+          } else if (ref_vel < 49.5) {
+            ref_vel += 0.224;
+          }
 
           // Vector of widely spaced (x, y) waypoints, evenly spaced at 30m
           // These waypoints are later interpolated with a spline
@@ -243,6 +276,6 @@ int main() {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
+
   h.run();
 }
